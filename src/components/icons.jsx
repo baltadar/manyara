336d4c6412ec89c67a97246ad888d60b25c8775e
baltadar <josephrichards293@gmail.com>
2025-03-15 18:44:00 +0300

    Create icons.jsx
    Minor edits

---
+++ src/components/icons.jsx
@@ -0,0 +1,12 @@
+// src/components/Icons.jsx
+import React from 'react';
+import { FaUserTie, FaBriefcase, FaGraduationCap, FaQuoteLeft } from "react-icons/fa";
+
+const Icons = {
+  FaUserTie,
+  FaBriefcase,
+  FaGraduationCap,
+  FaQuoteLeft
+};
+
+export default Icons;
